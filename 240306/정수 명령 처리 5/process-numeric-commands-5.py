635550ef531eb22bb59d4eb070d3430aed50b4d4
youngjuKim <201900931@hufs.ac.kr>
2024-03-06 16:42:15 +0900

    Title: 정수 명령 처리 5, Time: 409ms, Memory: 28MB, Status: Passed - CodeTree

--- 240306/정수 명령 처리 5/process-numeric-commands-5.py
+++ 240306/정수 명령 처리 5/process-numeric-commands-5.py
@@ -1,17 +1,17 @@
 N = int(input())
-    res = []
+res = []
 
-    for _ in range(N):
-        tmp = input().split()
-        command = tmp[0]
-        if len(tmp) == 2:
-            n = int(tmp[1])
+for _ in range(N):
+    tmp = input().split()
+    command = tmp[0]
+    if len(tmp) == 2:
+        n = int(tmp[1])
 
-        if command == "push_back":
-            res.append(n)
-        elif command == "pop_back":
-            res.pop() # 맨 뒤의 요소 삭제
-        elif command == "size":
-            print(len(res))
-        elif command == "get":
-            print(res[n-1])
+    if command == "push_back":
+        res.append(n)
+    elif command == "pop_back":
+        res.pop() # 맨 뒤의 요소 삭제
+    elif command == "size":
+        print(len(res))
+    elif command == "get":
+        print(res[n-1])
